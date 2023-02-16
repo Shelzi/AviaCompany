b043a939d89b90cf2abce3b5f24140ec7af623aa
Aleksej Bogdanik <abogdanik.laba@solvd.com>
2023-02-17 02:42:49 +0300

    ModelsAdjust

--- src/main/java/com/solvd/AviaCompany/hierarchy/Country.java
+++ src/main/java/com/solvd/AviaCompany/hierarchy/Country.java
@@ -1,10 +1,12 @@
 package com.solvd.AviaCompany.hierarchy;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.List;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Country {
 
     private int id;
--- src/main/java/com/solvd/AviaCompany/hierarchy/Passenger.java
+++ src/main/java/com/solvd/AviaCompany/hierarchy/Passenger.java
@@ -1,10 +1,12 @@
 package com.solvd.AviaCompany.hierarchy;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.List;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Passenger {
 
     private int id;
